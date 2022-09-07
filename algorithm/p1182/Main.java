6fe677ee3d3a7a42d083b6ea95c3816a02b025bd
hjwook <45505587+hjwook@users.noreply.github.com>
2022-09-07 23:22:18 +0900

    testCommit

--- algorithm/p1182/Main.java
+++ algorithm/p1182/Main.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Main {
-
+//test
     static int combi(int n, int r) {
         if (n == r || r == 0)
             return 1;
