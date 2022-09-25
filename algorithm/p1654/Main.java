bcbbbfadcdf1734ac0c85cd71f60f4813aca12e7
hjwook <hjwook1222@naver.com>
2022-09-25 16:35:01 +0900

    220925

--- algorithm/p1654/Main.java
+++ algorithm/p1654/Main.java
@@ -1,29 +1,50 @@
 package p1654;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Main {
 
-  //Scanner 이용시 시간이 초과하여 BufferdWrtier 사용
-  static BufferedWriter bw = new BufferedWriter(
-    new OutputStreamWriter(System.out)
-  );
+  public static int[] lines;
+  public static long max = 0;
+  public static long min = 0;
+  public static int K;
+  public static int N;
 
   public static void main(String args[]) throws IOException {
     Scanner sc = new Scanner(System.in);
 
-    int K = sc.nextInt();
-    int N = sc.nextInt();
+    K = sc.nextInt();
+    N = sc.nextInt();
 
-    int[] lines = new int[K];
+    lines = new int[K];
 
+    //max값은 랜선 중 가장 긴 값으로.
     for (int i = 0; i < K; i++) {
       lines[i] = sc.nextInt();
+      if (max < lines[i]) {
+        max = lines[i];
+      }
     }
+    max++; //max 를 1을 더 잡아 mid 가 0 으로 나뉘어지는 경우 방지.
+    long mid = 0;
+    while (min < max) {
+      mid = (max + min) / 2;
+      int possibleLines = 0;
+
+      for (int i = 0; i < lines.length; i++) {
+        possibleLines += lines[i] / mid;
+      }
+      if (possibleLines >= N) {
+        min = mid + 1;
+      } else {
+        max = mid;
+      }
+    }
+    //1을 + 했던걸 다시 빼줌
+    System.out.println(min - 1);
+
     sc.close();
   }
 }
-//-10 -10 2 3 3 6 7 10 10 10
+//802 -> 401 -> 200
