72bf29d7f34bc76b3371134953c324063f65c1f9
hjwook <hjwook1222@naver.com>
2022-10-03 19:24:39 +0900

    221003

---
+++ algorithm/p12015/Main.java
@@ -0,0 +1,20 @@
+package p12015;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] A = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      A[i] = sc.nextInt();
+    }
+
+    sc.close();
+  }
+}
+//
