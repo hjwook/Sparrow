2e4d82132d5eed64e423fd72b8866c2e1470cef8
hjwook <hjwook1222@naver.com>
2022-10-05 23:56:37 +0900

    221005

--- algorithm/p9012/Main.java
+++ algorithm/p9012/Main.java
@@ -5,13 +5,30 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
 
-    public static boolean isVPS(String input) {
-        return false;
+    public static String isVPS(String input) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < input.length(); i++) {
+            Character c = input.charAt(i);
+
+            if (c == '(') {
+                stack.push(input.charAt(i));
+            } else {
+                if (stack.isEmpty()) {
+                    return "NO";
+                }
+                stack.pop();
+            }
+        }
+        if (stack.isEmpty()) {
+            return "YES";
+        } else {
+            return "NO";
+        }
     }
 
     public static void main(String args[]) throws IOException {
@@ -21,7 +38,10 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < T; i++) {
-            wr.write(Boolean.toString(isVPS(br.readLine())) + "\n");
+            wr.write(isVPS(br.readLine()) + "\n");
         }
+        wr.flush();
+        wr.close();
+
     }
 }
