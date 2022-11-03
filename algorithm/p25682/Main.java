10b1bc3095add8a9e02cc8473bbcbae25f55c77a
hjwook <hjwook1222@naver.com>
2022-11-04 00:18:51 +0900

    chess2

--- algorithm/p25682/Main.java
+++ algorithm/p25682/Main.java
@@ -3,7 +3,7 @@ package p25682;
 import java.util.Scanner;
 
 public class Main {
-    public static Character[][] board;
+    public static boolean[][] board;
     public static int min = 2000 * 2000;
     public static int K;
 
@@ -11,18 +11,21 @@ public class Main {
 
         int count = 0;
         // 마스크의 첫번째 칸의 색이 기준
-        Character base = board[x1][y1];
+        boolean base = board[x1][y1];
         for (int i = x1; i < x2; i++) {
             for (int j = y1; j < y2; j++) {
                 // 'W' 차례에 'B' 가 오거나, 'B' 차례에 'W'가 오면 count++
-                if (board[i][j] != base) {
+                boolean cur = board[i][j];
+                if (cur != base) {
                     count++;
                 }
                 // 다음칸은 색이 바뀌므로, 검사할 색 change
-                base = (base == 'W' ? 'B' : 'W');
+                base = !base;
             }
-            // 줄바꿈시 색 change
-            // base = (base == 'W' ? 'B' : 'W');
+            // 줄바꿈시 색 change 는 K가 짝수일경우만
+            if (K % 2 == 0) {
+                base = !base;
+            }
         }
 
         count = Math.min(count, K * K - count);// 첫번째 색이 유지되냐 반전되냐 중 count 적은것 선택
@@ -37,11 +40,16 @@ public class Main {
         int M = sc.nextInt();
         K = sc.nextInt();
         sc.nextLine();
-        board = new Character[N][M];
+        board = new boolean[N][M];
         for (int i = 0; i < N; i++) {
             String input = sc.nextLine();
             for (int j = 0; j < M; j++) {
-                board[i][j] = input.charAt(j);
+                // White = T Black = F
+                if (input.charAt(j) == 'W') {
+                    board[i][j] = true;
+                } else {
+                    board[i][j] = false;
+                }
             }
         }
 
