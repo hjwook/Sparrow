6841ed6ef0e9c8d3d6620d4e8c75be148f043796
hjwook <hjwook1222@naver.com>
2022-11-10 23:18:59 +0900

    221110_fileAssemble

--- algorithm/p11066/Main.java
+++ algorithm/p11066/Main.java
@@ -3,6 +3,7 @@ package p11066;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class Main {
     public static int[] fileSize;
@@ -22,12 +23,24 @@ public class Main {
             String input = br.readLine();
             String[] inputs = input.split(" ");
 
+            int cost = 0;
             fileSize = new int[K];
+
+            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
             for (int j = 0; j < inputs.length; j++) {
                 fileSize[j] = Integer.parseInt(inputs[j]);
+                minHeap.add(Integer.parseInt(inputs[j]));
             }
 
-            sb.append(minCost(K));
+            while (minHeap.size() > 1) {
+                int num1 = minHeap.poll();
+                int num2 = minHeap.poll();
+
+                cost += (num1 + num2);
+                minHeap.add(num1 + num2);
+            }
+            sb.append(cost).append("\n");
+            // sb.append(minCost(K));
 
         }
 
@@ -35,3 +48,19 @@ public class Main {
         br.close();
     }
 }
+// 30 30 40 50 20 20
+// 40
+// 30 30 40 50
+// 40 60
+// 40 50
+// 80 60
+// 50
+// 80 110
+// 190
+
+// 3+6+9+6+9 = 33
+// 1 2 3 4 5
+// 3 3 4 5
+// 6 4 5
+// 6 9
+// 15
