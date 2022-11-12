86241678a78f49a30401dc2123a9f110eed86206
hjwook <hjwook1222@naver.com>
2022-11-12 18:37:39 +0900

    221112

--- algorithm/p11066/Main.java
+++ algorithm/p11066/Main.java
@@ -3,14 +3,36 @@ package p11066;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 
 public class Main {
     public static int[] fileSize;
+    public static int[][] memo;
 
-    public static int minCost(int index) {
+    public static int minCost(int start, int end) {
+        // start 가 end 보다 크면 return 0
+        if (start > end) {
+            return 0;
+        }
+        // start 와 end index 같으면 단일값
+        else if (start == end) {
+            return fileSize[start];
+        }
+        // 차이가 1이면 단순히 두개 더한값
+        else if (Math.abs(start - end) <= 1) {
+            return fileSize[start] + fileSize[end];
+        }
 
-        return 0;
+        if (memo[start][end] == 0) {
+            // 자르는 기준 정할 mask 크기
+            for (int i = 2; i < fileSize.length - 1; i++) {
+                // 정해진 mask 크기로 fileSize 배열 iterate
+                for (int j = 0; j < fileSize.length - i; j++) {
+                    memo[start][end] = minCost(0, j - 1) + minCost(j, j + i - 1) + minCost(j + i, fileSize.length - 1);
+                }
+            }
+
+        }
+        return memo[start][end];
     }
 
     public static void main(String args[]) throws IOException {
@@ -23,25 +45,14 @@ public class Main {
             String input = br.readLine();
             String[] inputs = input.split(" ");
 
-            int cost = 0;
             fileSize = new int[K];
-
-            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+            memo = new int[K][K];
             for (int j = 0; j < inputs.length; j++) {
                 fileSize[j] = Integer.parseInt(inputs[j]);
-                minHeap.add(Integer.parseInt(inputs[j]));
+
             }
 
-            while (minHeap.size() > 1) {
-                int num1 = minHeap.poll();
-                int num2 = minHeap.poll();
-
-                cost += (num1 + num2);
-                minHeap.add(num1 + num2);
-            }
-            sb.append(cost).append("\n");
-            // sb.append(minCost(K));
-
+            sb.append(minCost(0, K - 1)).append("\n");
         }
 
         System.out.println(sb);
