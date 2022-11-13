75749973e6adebb13305ed8dc8dd4a727e13d824
hjwook <hjwook1222@naver.com>
2022-11-13 21:06:04 +0900

    221113_Dp2_1

--- algorithm/p11066/Main.java
+++ algorithm/p11066/Main.java
@@ -5,59 +5,66 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static int[] fileSize;
-    public static int[][] memo;
 
-    public static int minCost(int start, int end) {
-        // start 가 end 보다 크면 return 0
-        if (start > end) {
-            return 0;
-        }
-        // start 와 end index 같으면 단일값
-        else if (start == end) {
-            return fileSize[start];
-        }
-        // 차이가 1이면 단순히 두개 더한값
-        else if (Math.abs(start - end) <= 1) {
-            return fileSize[start] + fileSize[end];
-        }
+  public static int[] fileSize;
+  public static int[] fileSum;
+  public static int[][] dp;
 
-        if (memo[start][end] == 0) {
-            // 자르는 기준 정할 mask 크기
-            for (int i = 2; i < fileSize.length - 1; i++) {
-                // 정해진 mask 크기로 fileSize 배열 iterate
-                for (int j = 0; j < fileSize.length - i; j++) {
-                    memo[start][end] = minCost(0, j - 1) + minCost(j, j + i - 1) + minCost(j + i, fileSize.length - 1);
-                }
-            }
-
-        }
-        return memo[start][end];
+  //start~end 까지 합치는 비용
+  public static int minCost(int start, int end) {
+    // start 가 end 보다 크거나 자기자신 합치는 비용 0
+    if (start >= end) {
+      return 0;
+    }
+    // 차이가 1이면 단순히 두개 더한값
+    else if (Math.abs(start - end) <= 1) {
+      return fileSize[start] + fileSize[end];
     }
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < T; i++) {
-            int K = Integer.parseInt(br.readLine());
-            String input = br.readLine();
-            String[] inputs = input.split(" ");
-
-            fileSize = new int[K];
-            memo = new int[K][K];
-            for (int j = 0; j < inputs.length; j++) {
-                fileSize[j] = Integer.parseInt(inputs[j]);
-
-            }
-
-            sb.append(minCost(0, K - 1)).append("\n");
-        }
-
-        System.out.println(sb);
-        br.close();
+    if (dp[start][end] == 0) {
+      int minCost = Integer.MAX_VALUE;
+      // 자르는 기준 i
+      for (int i = start; i < end; i++) {
+        //자르는기준 왼쪽 + 자르는기준 오른쪽 + 구간합 중 min 값이 dp[start][end]
+        int temp =
+          minCost(start, i) +
+          minCost(i + 1, end) +
+          fileSum[end + 1] -
+          fileSum[start];
+        minCost = Math.min(minCost, temp);
+      }
+      dp[start][end] = minCost;
     }
+    return dp[start][end];
+  }
+
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int T = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < T; i++) {
+      int K = Integer.parseInt(br.readLine());
+      String input = br.readLine();
+      String[] inputs = input.split(" ");
+
+      fileSize = new int[K];
+      fileSum = new int[K + 1];
+      dp = new int[K][K];
+      for (int j = 0; j < inputs.length; j++) {
+        fileSize[j] = Integer.parseInt(inputs[j]);
+        fileSum[j + 1] += fileSum[j] + fileSize[j];
+        // for (int k = 0; k < K; k++) {
+        //   dp[j][k] = Integer.MAX_VALUE;
+        // }
+      }
+
+      sb.append(minCost(0, K - 1)).append("\n");
+    }
+
+    System.out.println(sb);
+    br.close();
+  }
 }
 // 30 30 40 50 20 20
 // 40
@@ -68,7 +75,6 @@ public class Main {
 // 50
 // 80 110
 // 190
-
 // 3+6+9+6+9 = 33
 // 1 2 3 4 5
 // 3 3 4 5
