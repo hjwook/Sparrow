6028ec47a792222a14c8d5c91bc98cada411a6bb
hjwook <hjwook1222@naver.com>
2022-11-15 00:30:25 +0900

    221115_dp_2_3_solve

--- algorithm/p1520/Main.java
+++ algorithm/p1520/Main.java
@@ -5,31 +5,68 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static int[][] map;
+    public static int[][] dp;
+    public static int M;
+    public static int N;
 
-    public static int cntRoute() {
-        return 0;
+    // 0,0 ~ i-1,j-1 까지 가는 경로의 갯수 (마스킹 씌워서, 1,1~i,j 까지 가는 경로 갯수)
+    public static int cntRoute(int i, int j) {
+        // 마스킹된곳 까지 경로는 0개
+        if (i == 0 || i == M + 1 || j == 0 || j == N + 1) {
+            return 0;
+
+        }
+        // 1,1 ~ 1,1 까지 1개
+        else if (i == 1 && j == 1) {
+            return 1;
+        }
+        if (dp[i][j] == -1) {
+            int temp = 0;
+            if (map[i - 1][j] > map[i][j]) {
+                temp += cntRoute(i - 1, j);
+            }
+            if (map[i + 1][j] > map[i][j]) {
+                temp += cntRoute(i + 1, j);
+            }
+            if (map[i][j - 1] > map[i][j]) {
+                temp += cntRoute(i, j - 1);
+            }
+            if (map[i][j + 1] > map[i][j]) {
+                temp += cntRoute(i, j + 1);
+            }
+            dp[i][j] = temp;
+        }
+        return dp[i][j];
     }
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         String input = br.readLine();
         String[] inputs = input.split(" ");
 
-        int M = Integer.parseInt(inputs[0]);
-        int N = Integer.parseInt(inputs[1]);
+        M = Integer.parseInt(inputs[0]);
+        N = Integer.parseInt(inputs[1]);
 
-        int[][] map = new int[M][N];
-        for (int i = 0; i < M; i++) {
+        map = new int[M + 2][N + 2];
+        dp = new int[M + 2][N + 2];
+
+        for (int i = 0; i < M + 2; i++) {
+            for (int j = 0; j < N + 2; j++) {
+                map[i][j] = 10001;
+                dp[i][j] = -1;
+            }
+        }
+        for (int i = 1; i <= M; i++) {
             input = br.readLine();
             inputs = input.split(" ");
 
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(inputs[j]);
+            for (int j = 1; j <= N; j++) {
+                map[i][j] = Integer.parseInt(inputs[j - 1]);
             }
         }
-        System.out.println(cntRoute());
+        System.out.println(cntRoute(M, N));
         br.close();
     }
 }
