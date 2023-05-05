4cfc2c77c3926a15ad4e312dcbe7e07935ab0c9f
hjwook <45505587+hjwook@users.noreply.github.com>
2023-05-05 21:29:17 +0900

    [Silver II] Title: 스택 수열, Time: 968 ms, Memory: 107244 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int n = sc.nextInt();
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        // 가장 높았던 수 저장
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            int input = sc.nextInt();
+            // max보다 입력이 크다면 차이만큼 push
+            if (max < input) {
+                for (int j = max + 1; j <= input; j++) {
+                    stack.push(j);
+                    sb.append('+').append('\n');
+                }
+                max = input;
+            }
+            // 아닌경우엔, stack의 top 과 입력 일치하지 않으면 불가
+            else if (stack.peek() != input) {
+                System.out.println("NO");
+                sc.close();
+                return;
+            }
+            stack.pop();
+            sb.append('-').append('\n');
+        }
+
+        System.out.println(sb);
+        sc.close();
+    }
+}
