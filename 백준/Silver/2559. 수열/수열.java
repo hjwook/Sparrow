4f35eddb4ac620c4c70506827ff95017fb206d70
hjwook <45505587+hjwook@users.noreply.github.com>
2023-05-05 22:40:04 +0900

    [Silver III] Title: 수열, Time: 816 ms, Memory: 111292 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] arr = new int[N];
+        int[] pSum = new int[N + 1];
+        // 먼저 누적합 배열 생성
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+            pSum[i + 1] = pSum[i] + arr[i];
+        }
+        // K일 연속 온도합 배열 생성
+        int[] kpSum = new int[N + 1];
+        int max = Integer.MIN_VALUE;
+        for (int i = K; i < N + 1; i++) {
+            kpSum[i] = pSum[i] - pSum[i - K];
+            if (max < kpSum[i]) {
+                max = kpSum[i];
+            }
+        }
+        System.out.println(max);
+        sc.close();
+    }
+}
