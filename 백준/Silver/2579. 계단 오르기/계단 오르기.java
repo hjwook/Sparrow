05aa25a3bd02facf7a08d598ca88dde1e8821921
hjwook <45505587+hjwook@users.noreply.github.com>
2023-05-09 21:54:27 +0900

    [Silver III] Title: 계단 오르기, Time: 236 ms, Memory: 18056 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+	public static int [] stair = new int [301];
+	public static int [] memo = new int [301];
+	
+	public static int cost (int floor){
+		if(floor <0) return 0;
+		else if(floor == 0) return stair[0];
+		else if (floor ==1) return stair[0]+stair[1];
+		
+		if(memo[floor] ==0 ) {
+			memo[floor] = Math.max(cost(floor-2) + stair[floor], cost(floor-3)+stair[floor-1]+stair[floor]);
+		}
+		
+		return memo[floor];
+		
+	}
+	public static void main (String args[]){
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=0; i<n; i++){
+			stair[i] = sc.nextInt();
+		}
+
+		System.out.println(cost(n-1));
+		
+		sc.close();
+		
+	}
+}
